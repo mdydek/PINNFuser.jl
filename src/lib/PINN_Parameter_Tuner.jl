6c081cc1faa5c46ec6508994204571d1cd3f45e8
wiktorperczak <wiktor.prczk@gmail.com>
2025-11-20 15:29:23 +0100

    Library function for parameter tuning

---
+++ src/lib/PINN_Parameter_Tuner.jl
@@ -0,0 +1,116 @@
+module PINNParamTuner
+
+using OrdinaryDiffEq
+using Lux
+using ComponentArrays
+using Optimization
+using OptimizationOptimisers
+using Statistics
+
+export PINN_Parameter_Optimizer
+
+function PINN_Parameter_Optimizer(
+    model_ode!::Function,
+    u0::AbstractVector{<:Real},
+    tspan::Tuple,
+    target_data::AbstractMatrix;
+    initial_params::Vector{<:Real},
+    tune_params::Vector{Int},
+    range_fraction::Float64 = 0.6,
+    learning_rate::Float64 = 0.01,
+    optimizer = ADAM,
+    iters::Int = 1000,
+    rng = StableRNG(1234),
+    num_param_samples::Int = 5,
+    nn = nothing
+)
+
+    nin = length(u0)
+    nout = length(tune_params)
+
+    # Default NN if none provided
+    if nn === nothing
+        nn = Lux.Chain(
+            Lux.Dense(nin, 32, tanh),
+            Lux.Dense(32, 32, tanh),
+            Lux.Dense(32, nout)
+        )
+    end
+
+    nn_params, st = Lux.setup(rng, nn)
+    nn_params = 0 * ComponentVector{Float64}(nn_params)
+    
+    tsteps = range(tspan[1], tspan[2], length=size(target_data,1))
+
+    function param_from_NN(u, p, st)
+        y, _ = nn(u, p, st)
+        pars = similar(y, length(initial_params))
+
+        for (k, idx) in enumerate(tune_params)
+            base = initial_params[idx]
+            minv = base * (1 - range_fraction)
+            maxv = base * (1 + range_fraction)
+            pars[idx] = minv + abs(y[k]) * (maxv - minv)
+        end
+
+        for i in 1:length(initial_params)
+            if !(i in tune_params)
+                pars[i] = initial_params[i]
+            end
+        end
+
+        return pars
+    end
+
+    function wrapped_ODE!(du, u, p, t)
+        p_new = param_from_NN(u, p, st)
+        model_ode!(du, u, p_new, t)
+    end
+
+    function predict(nn_p)
+        prob = ODEProblem(wrapped_ODE!, u0, tspan, nn_p)
+        solve(prob, Vern7(), dtmax=1e-2, saveat=tsteps, reltol=1e-6, abstol=1e-6)
+    end
+
+    function loss(p)
+        sol = predict(p)
+        pred = Array(sol)
+        ydim = min(size(pred,1), size(target_data,2))
+
+        pred_slice = pred[1:ydim, :]'
+        true_slice = target_data[:, 1:ydim]
+
+        mean(abs2, pred_slice .- true_slice)
+    end
+
+    function make_callback()
+        iter = Ref(0)
+        return function (p, l)
+            if iter[] % 1 == 0
+                println("iter $(iter[]) | loss = $(round(l, sigdigits=5))")
+            end
+            iter[] += 1
+            return false
+        end
+    end
+    cb = make_callback()
+
+    adtype = Optimization.AutoForwardDiff()
+    optf = Optimization.OptimizationFunction((x,p)->loss(x), adtype)
+    optprob = Optimization.OptimizationProblem(optf, nn_params)
+
+    res = Optimization.solve(optprob, optimizer(learning_rate),
+                             callback=cb, maxiters=iters)
+
+
+    tuned_list = Vector{Vector{Float64}}(undef, num_param_samples)
+
+    for i in 1:num_param_samples
+        u_sample = target_data[i, 1:length(u0)]
+        tuned_list[i] = param_from_NN(u_sample, res.u, st)
+    end
+
+    return trained, st, tuned_list
+end
+
+end
--- src/lib/lib.jl
+++ src/lib/lib.jl
@@ -3,7 +3,7 @@ module LibInfuser
 include("PINN_Infuser.jl")
 include("PINN_Extrapolator.jl")
 include("PINN_Symbolic_Regressor.jl")
-include("PINN_Parameter_Optimizer.jl")
+include("PINN_Parameter_Tuner.jl")
 include("PINN_Plotter.jl")
 
 end
